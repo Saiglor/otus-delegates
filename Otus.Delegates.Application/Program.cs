1b844948cf044be03bd46f32076360d9c87bec5b
Saiglor <jakimets.2012@gmail.com>
2022-01-02 17:31:56 +0500

    Init project

---
+++ Otus.Delegates.Application/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System;
+
+namespace Otus.Delegates.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
